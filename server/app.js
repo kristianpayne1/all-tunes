1e39d49577c0f6b4ed982a885aa66d6cee8bce79
Kristian Payne <kjp29@kent.ac.uk>
2020-08-18 18:05:47 +0100

    Create simple server clients can host, join and disconnect

---
+++ server/app.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const express = require('express');
+const WebSocket = require('ws');
+const server = require('http').createServer();
+const app = require('./http-server');
+const crypto = require('crypto');
+
+const port = process.env.PORT || 8524;
+
+server.on('request', app);
+
+const wss = new WebSocket.Server({ server });
+
+// maps rooms to list of clients
+let parties = new Map();
+// maps room to host
+let partyHost = new Map();
+
+wss.on('connection', (ws, req) => {
+    ws.ip = req.socket.remoteAddress;
+    ws.inParty = false;
+    ws.partyCode = '';
+    // receiving a message
+    ws.on('message', (message) => {
+        var data = JSON.parse(message);
+        
+        switch (data.messageType) {
+            case 'CREATE_PARTY': {
+                // create random 6 char party code
+                ws.partyCode = crypto.randomBytes(20).toString('hex').substring(0, 6).toUpperCase();
+
+                console.log("Party created with party code: " + ws.partyCode);
+
+                // create party 
+                parties.set(ws.partyCode, []);
+                partyHost.set(ws.partyCode, ws);
+                // respond to host with party code
+                const response = {
+                    messageType: 'CREATE_PARTY_SUCCESS',
+                    partycode: ws.partyCode
+                };
+                ws.send(JSON.stringify(response));
+            }
+            case 'JOIN_PARTY_REQUEST': {
+                if (parties.has(ws.partyCode)) {
+                    if (isClientInParty(ws.partyCode, ws.ip)) {
+                        console.log("Client: " + ws.ip + " joined party: " + ws.partyCode);
+
+                        // add client to party
+                        ws.inParty = true;
+                        let clients = parties.get(ws.partyCode);
+                        clients.push(ws);
+                        parties.set(ws.partyCode, ws.ip);
+                        const response = {
+                            messageType: 'JOINED_PARTY',
+                            partycode: ws.partyCode,
+                        };
+                        ws.send(JSON.stringify(response));
+                    } else {
+                        const response = {
+                            messageType: 'JOIN_PARTY_ERROR',
+                            error: 'Client already in party',
+                            partyCode: ws.partyCode
+                        }
+                        ws.send(JSON.stringify(response));
+                    }
+                } else {
+                    // tried joining unknown party
+                    const response = {
+                        messageType: 'JOIN_PARTY_ERROR',
+                        error: 'No party found',
+                        partyCode: ws.partyCode
+                    };
+                    ws.send(JSON.stringify(response));
+                }
+            }
+            case 'DISCONNECTED': {
+                leaveParty(ws);
+            }
+        }
+    });
+});
+
+function isClientInParty(party, ip)
+{
+    const clients = parties.get(party);
+    if (clients === undefined) {
+        return false;
+    } else {
+        return clients.find((item) => item.ip === ip) ? true : false;
+    }
+};
+
+function leaveParty(ws) {
+    if (ws.inParty === true) {
+        // Remove the player from the party
+        const clients = parties.get(ws.party);
+        clients.splice(clients.indexOf(ws.ip), 1);
+        parties.set(ws.party, clients);
+    }
+};
+
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
