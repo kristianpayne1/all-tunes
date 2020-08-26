8d62b1bae227c9a3c65feafa40c88db800c5f2ac
Kristian Payne <kjp29@kent.ac.uk>
2020-08-26 21:30:53 +0100

    Add party view and backend Web API support

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,29 +1,91 @@
 import React, { Component } from 'react';
-import SpotifyWebApi from 'spotify-web-api-js';
 import { Redirect } from 'react-router-dom';
-import { Table, Button } from 'react-bootstrap';
-
-const spotifyApi = new SpotifyWebApi();
+import PartyForm from './PartyForm.js';
+import PartyView from './PartyView.js';
+//import { Table, Button } from 'react-bootstrap';
 
 class Home extends Component {
     constructor() {
         super();
         const params = this.getHashParams();
         const token = params.access_token;
-        if (token) {
-            spotifyApi.setAccessToken(token);
-        }
         this.state = {
             loggedIn: token ? true : false,
-            topArtists: [],
-            topSongs: [],
-            recommendedSongs: [],
+            socket: null,
+            token: token,
+            isHost: false,
+            partyCode: '',
+            isConnected: false,
         }
     }
 
     componentDidMount() {
-        this.getTopArtists();
-        this.getTopSongs();
+        this.connect();
+    }
+
+    connect = () => {
+        if (this.state.loggedIn) {
+            let socket = new WebSocket("ws://localhost:8888");
+            this.setState({ socket: socket });
+            console.log('Connected to socket');
+
+            let self = this;
+
+            socket.onmessage = (event) => {
+                const message = JSON.parse(event.data);
+                console.log(message);
+                switch (message.messageType) {
+                    case 'SEND_ACCESS_TOKEN': {
+                        var response = {
+                            messageType: 'ACCESS_TOKEN',
+                            access_token: self.state.token
+                        };
+                        try {
+                            self.state.socket.send(JSON.stringify(response));
+                        } catch (error) {
+                            console.log(error);
+                        }
+                    }
+                    break;
+                    case 'CREATE_PARTY_SUCCESS': {
+                        let partyCode = message.partyCode;
+                        console.log('Hosting party: ' + partyCode);
+                        self.setState({ partyCode: partyCode, isHost: true, isConnected: true});
+                    }
+                        break;
+                    case 'JOINED_PARTY': {
+                        let partyCode = message.partyCode;
+                        console.log('Joined party: ' + partyCode);
+                        self.setState({ partyCode: partyCode, isConnected: true});
+                    }
+                        break;
+                    case 'JOIN_PARTY_ERROR': {
+                        self.setState({isConnected: false, partyCode: '', isHost: false});
+                        console.log("Failed to join party \n" + message.error)
+                    }
+                        break;
+                    default: {
+                        console.log("Recieved unknown message");
+                    }
+                }
+
+                socket.onerror = err => {
+                    console.error(
+                        "Socket encountered error: ",
+                        err.message,
+                        "Closing socket"
+                    );
+                    self.setState({isConnected: false, partyCode: '', isHost: false});
+                    socket.close();
+                }
+
+                socket.onclose = () => {
+                    self.setState({isConnected: false, partyCode: '', isHost: false});
+                    console.log('disconnected')
+                    // automatically try to reconnect on connection loss
+                }
+            }
+        }
     }
 
     getHashParams = () => {
@@ -38,230 +100,15 @@ class Home extends Component {
         return hashParams;
     }
 
-    getTopArtists = () => {
-        spotifyApi.getMyTopArtists({ time_range: 'long_term', limit: 50 })
-            .then((response) => {
-                console.log(response);
-                let topArtists = [];
-                response.items.forEach(artist => {
-                    topArtists.push(artist);
-                });
-                this.setState({ topArtists: topArtists });
-            })
-    }
-
-    renderTopArtists = () => {
-        return this.state.topArtists.map((artist, index) => {
-            let genres = '';
-            artist.genres.forEach((genre) => {
-                genres += genre + ', '
-            })
-            const { id, name } = artist //destructuring
-            return (
-                <tr key={id}>
-                    <td>{index + 1}</td>
-                    <td>{name}</td>
-                    <td>{genres}</td>
-                </tr>
-            )
-        })
-    }
-
-    getTopSongs = () => {
-        spotifyApi.getMyTopTracks({ time_range: 'long_term', limit: 50 })
-            .then((response) => {
-                console.log(response);
-                let topSongs = [];
-                response.items.forEach(song => {
-                    topSongs.push(song);
-                });
-                this.setState({ topSongs: topSongs });
-            })
-    }
-
-    renderTopSongs = () => {
-        return this.state.topSongs.map((song, index) => {
-            let artists = '';
-            song.artists.forEach((artist) => {
-                artists += artist.name + ', '
-            })
-            const { id, name, popularity } = song //destructuring
-            return (
-                <tr key={id}>
-                    <td>{index + 1}</td>
-                    <td>{name}</td>
-                    <td>{artists}</td>
-                    <td>{popularity}</td>
-                </tr>
-            )
-        })
-    }
-
-    getRecommendations = () => {
-        //let genres = this.getTopGenres();
-
-        let seedArtists = '';
-        //let seedSongs = '';
-        //let seedGenres = '';
-        for (let i = 0; i < 5; i++) {
-            seedArtists += this.state.topArtists[i].id + ',';
-        }
-
-        // for (let i = 0; i < 5; i++) {
-        //     seedGenres += genres[i] + ',';
-        // }
-
-        spotifyApi.getRecommendations({ seed_artists: seedArtists, min_danceability: 0.75, min_energy: 0.75, min_popularity: 60, min_tempo: 80 })
-            .then((response) => {
-                console.log(response);
-                let recommendedSongs = [];
-                response.tracks.forEach((song) => {
-                    recommendedSongs.push(song);
-                })
-                let sortedSongs = this.sortRecommendedSongs(recommendedSongs);
-                this.setState({ recommendedSongs: sortedSongs });
-            })
-    }
-
-    sortRecommendedSongs = (songs) => {
-        let n = songs.length;
-        for (let i = 1; i < n; ++i) { 
-            let key = songs[i]; 
-            let j = i - 1; 
-  
-            while (j >= 0 && songs[j].popularity < key.popularity) { 
-                songs[j + 1] = songs[j]; 
-                j = j - 1; 
-            } 
-            songs[j + 1] = key; 
-        } 
-        return songs;
-    }
-
-    getTopGenres = () => {
-        // to be returned
-        let topGenres = [];
-        // map genres to their points
-        let genreMap = new Map();
-        // keep track of what artists have been already accounted for
-        let artists = [];
-
-        this.state.topArtists.forEach((artist) => {
-            artists.push(artist);
-            artist.genres.forEach((genre) => {
-                if (genreMap.has(genre)) {
-                    let points = genreMap.get(genre) + 1;
-                    genreMap.set(genre, points);
-                } else {
-                    genreMap.set(genre, 1);
-                }
-            })
-        })
-
-        this.state.topSongs.forEach((song) => {
-            song.artists.forEach((artist) => {
-                if (!artists.includes(artist)) {
-                    artists.push(artist);
-                    spotifyApi.getArtist(artist.id)
-                        .then((response) => {
-                            response.genres.forEach((genre) => {
-                                if (genreMap.has(genre)) {
-                                    let points = genreMap.get(genre) + 1;
-                                    genreMap.set(genre, points);
-                                } else {
-                                    genreMap.set(genre, 1);
-                                }
-                            })
-                        })
-                }
-            })
-        })
-
-        for (let i = 0; i < 5; i++) {
-            let currentTopGenrePoint = 0;
-            let currentTopGenre = '';
-            genreMap.forEach((value, key) => {
-                if (value > currentTopGenrePoint) {
-                    currentTopGenrePoint = value;
-                    currentTopGenre = key;
-                }
-            });
-            console.log(currentTopGenre + ' = ' + currentTopGenrePoint)
-            topGenres.push(currentTopGenre);
-            genreMap.delete(currentTopGenre);
-        }
-
-        return topGenres;
-    }
-
-    renderRecommendedSongs = () => {
-        return this.state.recommendedSongs.map((song, index) => {
-            let artists = '';
-            song.artists.forEach((artist) => {
-                artists += artist.name + ', '
-            })
-            const { id, name, popularity } = song //destructuring
-            return (
-                <tr key={id}>
-                    <td>{index + 1}</td>
-                    <td>{name}</td>
-                    <td>{artists}</td>
-                    <td>{popularity}</td>
-                </tr>
-            )
-        })
-    }
-
     render() {
         let redirect = !this.state.loggedIn ? <Redirect to="/" /> : null;
-        let recommendedTable = this.state.recommendedSongs.length !== 0 ?
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Recommended songs</th>
-                        <th>Artist(s)</th>
-                        <th>Popularity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.renderRecommendedSongs()}
-                </tbody>
-            </Table> :
-            null;
+        let partyView = this.state.isConnected ? 
+            <PartyView isHost={this.state.isHost} partyCode={this.state.partyCode}/> : 
+            <PartyForm socket={this.state.socket} />
         return (
             <div>
                 {redirect}
-                <h1>Home</h1>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Top artists</th>
-                            <th>Genres</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderTopArtists()}
-                    </tbody>
-                </Table>
-                <br />
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Top songs</th>
-                            <th>Artist(s)</th>
-                            <th>Popularity</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderTopSongs()}
-                    </tbody>
-                </Table>
-                <Button onClick={this.getRecommendations}>Get recommendations</Button>
-                <p>Get recommendated songs</p>
-                {recommendedTable}
+                {partyView}
             </div>
         )
     }
--- server/app.js
+++ server/app.js
@@ -1,147 +1,277 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
 
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
+var crypto = require('crypto');
 
-var client_id = '4553b87393ad47fcb7e22bda6e2c8b4d'; // Your client id
-var client_secret = '9705be4d156346139ab83e4c8fd0c9ae'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// SPOTIFY WEB API
+
+var client_id = '4553b87393ad47fcb7e22bda6e2c8b4d';
+var client_secret = '9705be4d156346139ab83e4c8fd0c9ae';
+var redirect_uri = 'http://localhost:8888/callback';
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 };
 
 var stateKey = 'spotify_auth_state';
 
 var app = express();
+var expressWs = require('express-ws')(app);
 
 app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
+    .use(cors())
+    .use(cookieParser());
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
 
-  var state = generateRandomString(16);
-  res.cookie(stateKey, state);
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
 
-  // your application requests authorization
-  var scope = 'user-top-read user-read-recently-played user-library-read';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+    // your application requests authorization
+    var scope = 'user-top-read user-read-recently-played user-library-read';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
 });
 
-app.get('/callback', function(req, res) {
+app.get('/callback', function (req, res) {
 
-  // your application requests refresh and access tokens
-  // after checking the state parameter
+    // your application requests refresh and access tokens
+    // after checking the state parameter
 
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    } else {
+        res.clearCookie(stateKey);
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
         };
 
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
+        request.post(authOptions, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+
+                var access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+
+                var options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                // use the access token to access the Spotify Web API
+                request.get(options, function (error, response, body) {
+                    console.log(body);
+                });
+
+                // we can also pass the token to the browser to make requests from there
+                res.redirect('http://localhost:3000/home/#' +
+                    querystring.stringify({
+                        access_token: access_token,
+                        refresh_token: refresh_token
+                    }));
+            } else {
+                res.redirect('/#' +
+                    querystring.stringify({
+                        error: 'invalid_token'
+                    }));
+            }
         });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/home/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
-});
-
-app.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
     }
-  });
 });
+
+app.get('/refresh_token', function (req, res) {
+
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        },
+        json: true
+    };
+
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var access_token = body.access_token;
+            res.send({
+                'access_token': access_token
+            });
+        }
+    });
+});
+
+// ------------------------------------------------------------
+// WEBSOCKET 
+
+// maps rooms to list of clients
+let parties = new Map();
+// maps room to host
+let partyHost = new Map();
+
+app.ws('/', function (ws, req) {
+    ws.ip = req.socket.remoteAddress;
+    ws.inParty = false;
+    ws.partyCode = '';
+    ws.spotifyWebApi = new SpotifyWebApi();
+    ws.topArtists = [];
+
+    // ask client for the spotify access_token
+    let response = {
+        messageType: 'SEND_ACCESS_TOKEN'
+    };
+    ws.send(JSON.stringify(response));
+
+    // receiving a message
+    ws.on('message', (message) => {
+        var data = JSON.parse(message);
+        switch (data.messageType) {
+            case 'ACCESS_TOKEN': {
+                if (data.access_token) {
+                    ws.spotifyWebApi.setAccessToken(data.access_token);
+                }
+            }
+                break;
+            case 'CREATE_PARTY': {
+                // create random 6 char party code
+                ws.partyCode = crypto.randomBytes(20).toString('hex').substring(0, 6).toUpperCase();
+
+                console.log("Party created with party code: " + ws.partyCode);
+
+                // create party 
+                parties.set(ws.partyCode, []);
+                partyHost.set(ws.partyCode, ws);
+
+                // respond to host with party code
+                const response = {
+                    messageType: 'CREATE_PARTY_SUCCESS',
+                    partyCode: ws.partyCode
+                };
+                ws.send(JSON.stringify(response));
+
+                // get clients top users
+                getTopArtists(ws);
+            }
+                break;
+            case 'JOIN_PARTY_REQUEST': {
+                console.log(data.partyCode);
+                if (parties.has(data.partyCode)) {
+                    if (isClientInParty(data.partyCode, ws.ip)) {
+                        ws.partyCode = data.partyCode;
+
+                        console.log("Client: " + ws.ip + " joined party: " + ws.partyCode);
+
+                        // add client to party
+                        ws.inParty = true;
+                        let clients = parties.get(ws.partyCode);
+                        clients.push(ws);
+                        parties.set(ws.partyCode, ws.ip);
+                        const response = {
+                            messageType: 'JOINED_PARTY',
+                            partyCode: ws.partyCode,
+                        };
+                        ws.send(JSON.stringify(response));
+
+                        // get clients top users
+                        getTopArtists();
+                    } else {
+                        const response = {
+                            messageType: 'JOIN_PARTY_ERROR',
+                            error: 'Client already in party'
+                        }
+                        ws.send(JSON.stringify(response));
+                    }
+                } else {
+                    // tried joining unknown party
+                    const response = {
+                        messageType: 'JOIN_PARTY_ERROR',
+                        error: 'No party found'
+                    };
+                    ws.send(JSON.stringify(response));
+                }
+            }
+                break;
+            case 'DISCONNECTED': {
+                leaveParty(ws);
+            }
+                break;
+        }
+    });
+});
+
+function getTopArtists(client) {
+    client.spotifyWebApi.getMyTopArtists({ time_range: 'long_term', limit: 50 })
+        .then(
+            function (data) {
+                let topArtists = [];
+                data.items.forEach(artist => {
+                    topArtists.push(artist);
+                });
+                client.topArtists = topArtists;
+            },
+            function (err) {
+                console.error(err);
+            }
+        );
+}
+
+function isClientInParty(party, ip) {
+    const clients = parties.get(party);
+    if (clients === undefined) {
+        return false;
+    } else {
+        return clients.find((item) => item.ip === ip) ? true : false;
+    }
+};
+
+function leaveParty(ws) {
+    if (ws.inParty === true) {
+        // Remove the player from the party
+        const clients = parties.get(ws.party);
+        clients.splice(clients.indexOf(ws.ip), 1);
+        parties.set(ws.party, clients);
+    }
+};
 
 console.log('Listening on 8888');
 app.listen(8888);
